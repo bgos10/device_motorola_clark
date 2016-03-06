0d7235647164befaa335208fc31b024d27ce2b8a
bgos10 <bgos10@yahoo.co.in>
2016-03-06 23:18:20 +0530

    Orion-ify

--- orion.mk
+++ orion.mk
@@ -6,10 +6,10 @@ TARGET_SCREEN_WIDTH := 2560
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/orion/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/orion/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/clark/aosp_clark.mk)
@@ -17,7 +17,7 @@ $(call inherit-product, device/motorola/clark/aosp_clark.mk)
 DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_clark
-PRODUCT_BRAND := Android
+PRODUCT_BRAND := Motorola
 PRODUCT_MODEL := XT1575
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+PRODUCT_NAME=orion_clark
