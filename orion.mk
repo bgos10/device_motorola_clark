fe77b15fa0e740e75a3550ec0e8e2b5cb071148c
bgos10 <bgos10@yahoo.co.in>
2016-03-07 23:27:06 +0530

    Add Maintainer & Fix Build Desc

--- orion.mk
+++ orion.mk
@@ -6,6 +6,7 @@ TARGET_SCREEN_WIDTH := 2560
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit some common CM stuff.
+$(call inherit-product, vendor/orion/config/common.mk)
 $(call inherit-product, vendor/orion/config/common_full_phone.mk)
 
 # Enhanced NFC
@@ -21,3 +22,9 @@ PRODUCT_BRAND := Motorola
 PRODUCT_MODEL := XT1575
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 PRODUCT_NAME=orion_clark
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+PRODUCT_NAME=clark \
+  	BUILD_FINGERPRINT="motorola/clark_retus/clark:6.0/MPH24.49-18/18:user/release-keys" \
+  	PRIVATE_BUILD_DESC="clark_retus-user 6.0 MPH24.49-18 18 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Bishal Goswami (bgos10)"
