c42906fd7106d127e97b8f8c2c433d30766df912
bgos10 <bgos10@yahoo.co.in>
2016-08-28 22:51:42 +0530

    Turbocharged

--- turbo.mk
+++ turbo.mk
@@ -5,19 +5,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/clark/device.mk)
 
 # Inherit some common Orion stuff.
-$(call inherit-product, vendor/orion/config/common_full_phone.mk)
+$(call inherit-product, vendor/turbo/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/orion/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/turbo/config/nfc_enhanced.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := orion_clark
+PRODUCT_NAME := turbo_clark
 PRODUCT_BRAND := Android
 PRODUCT_DEVICE := clark
 PRODUCT_MODEL := XT1575
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
-
-PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Bishal Goswami (bgos10)"
